041bc7f1024a4559928ebd3dc89a34c54548a2df
Raúl Andrés Argott Castro <raul.argott@aumenta.mx>
2022-03-01 11:50:48 -0600

    hero-title

--- src/app/herramientas/herramientas.module.ts
+++ src/app/herramientas/herramientas.module.ts
@@ -5,13 +5,15 @@ import { HerramientasRoutingModule } from './herramientas-routing.module';
 import { RadarConsultingComponent } from './radar-consulting/radar-consulting.component';
 import { RadarAcademyComponent } from './radar-academy/radar-academy.component';
 import { RadarConnectComponent } from './radar-connect/radar-connect.component';
+import { HeroTitleComponent } from './hero-title/hero-title.component';
 
 
 @NgModule({
   declarations: [
     RadarConsultingComponent,
     RadarAcademyComponent,
-    RadarConnectComponent
+    RadarConnectComponent,
+    HeroTitleComponent
   ],
   imports: [
     CommonModule,
