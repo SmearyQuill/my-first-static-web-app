7faed31a0319d3b1f02b3e3c492bc04ee3a74a97
Axel López Rodríguez <axel.lopez@aumenta.mx>
2022-03-08 12:58:14 -0600

    Presentacion de Radar Terminada

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { MainPageComponent } from './main-page/main-page.component';
+import { PresentacionComponent } from './presentacion/presentacion.component';
 
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    PresentacionComponent
   ],
   imports: [
     CommonModule,
