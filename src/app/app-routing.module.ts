b7c72c1f306ddabe6e48654e9ff40da482576fb3
Raúl Andrés Argott Castro <raul.argott@aumenta.mx>
2022-03-11 17:34:34 -0600

    pagina contactanos comenzada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContactanosComponent } from './Componentes/contactanos/contactanos.component';
+import { ContactanosComponent } from './contactanos/contactanos.component';
 const routes: Routes = [
   {
     path: '',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VersionesComponent } from './Componentes/versiones/versiones.component';
 import { HerramientasComponent } from './Componentes/herramientas/herramientas.component';
-import { ContactanosComponent } from './Componentes/contactanos/contactanos.component';
+import { ContactanosComponent } from './contactanos/contactanos.component';
 import { MenuComponent } from './Componentes/menu/menu.component';
 import { SabiasQueCardComponent } from './Componentes/sabias-que-card/sabias-que-card.component';
 import { TestimonioCardComponent } from './Componentes/testimonio-card/testimonio-card.component';
