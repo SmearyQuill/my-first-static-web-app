35e5938300bd9564c70c3db592880de1f275d2b6
Raúl Andrés Argott Castro <raul.argott@aumenta.mx>
2022-03-16 15:56:35 -0600

    carousel con jquery y fallas

--- src/app/home/sabias-que-carousel/sabias-que-carousel.component.ts
+++ src/app/home/sabias-que-carousel/sabias-que-carousel.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+declare var $: any;
 @Component({
   selector: 'app-sabias-que-carousel',
   templateUrl: './sabias-que-carousel.component.html',
@@ -29,7 +29,25 @@ export class SabiasQueCarouselComponent implements OnInit {
   }
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+    
+  ngAfterViewInit():void {
+
+    $('.show-neighbors').click(function(this: HTMLElement){
+      $('.carousel-item', '.show-neighbors').each(function(this: HTMLElement){
+        var next = $(this).next();
+        if (! next.length) {
+          next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+      }).each(function(this: HTMLElement){
+        var prev = $(this).prev();
+        if (! prev.length) {
+          prev = $(this).siblings(':last');
+        }
+        prev.children(':nth-last-child(2)').clone().prependTo($(this));
+      });
+    });
   }
 
 }
