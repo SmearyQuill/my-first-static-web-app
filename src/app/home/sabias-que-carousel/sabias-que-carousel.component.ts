509aedbc960e469d5553f02a3bc16ea8e6894b9e
Axel López Rodríguez <axel.lopez@aumenta.mx>
2022-03-31 17:14:32 -0600

    Cambios de Imagenes para ahorrar espacio de carga.

--- src/app/home/sabias-que-carousel/sabias-que-carousel.component.ts
+++ src/app/home/sabias-que-carousel/sabias-que-carousel.component.ts
@@ -7,27 +7,6 @@ declare var $: any;
   styleUrls: ['./sabias-que-carousel.component.css']
 })
 export class SabiasQueCarouselComponent implements OnInit {
-  firstCard = {
-    specialCaseText : '¿SABÍAS QUE...',
-    questionText : 'el 60% de la estadías de un vehículo es en actividades administrativas que nadie te paga?',
-    informationText : 'Mide y controla tus tiempos por procesos y así aumenta tu productividad.',
-    buttonText : 'AGENDA UNA REUNIÓN WEB',
-    imageSrc : '/assets/Grupo 522@2x.png'
-  }
-  secondCard = {
-    specialCaseText : '',
-    questionText : '',
-    informationText : '',
-    buttonText : '',
-    imgSrc : ''
-  }
-  thirdCard = {
-    specialCaseText : '',
-    questionText : '',
-    informationText : '',
-    buttonText : '',
-    imgSrc : ''
-  }
   sabiasQueList = model.SabiasQueCards;
   constructor() { }
 
