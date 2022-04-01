778daf0e97205c7112e3f63d27d91620d268ed13
Raúl Andrés Argott Castro <raul.argott@aumenta.mx>
2022-04-01 14:32:42 -0600

    test api

--- server.js
+++ server.js
@@ -5,12 +5,19 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit:"100mb"}));
 
-app.post('/send',(req, res) => {
+app.post('/api/send',(req, res) => {
     console.log(req.body);
     res.json({
-        'statusCode': 200,
-        'statusMessage': 'SUCCESS'
-    });
+      'statusCode': 200,
+      'statusMessage': 'SUCCESS'
+  });
+});
+
+app.get('/api/test',(req,res) => {
+  res.json({
+    'statusCode': 200,
+    'statusMessage': 'SUCCESS'
+});
 });
 
 app.listen(3000, (req,res) => {
