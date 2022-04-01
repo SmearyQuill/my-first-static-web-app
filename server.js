a4e5c1988242ce5734f43410895c94089634fc80
Raúl Andrés Argott Castro <raul.argott@aumenta.mx>
2022-04-01 17:55:11 -0600

    express hace de servidor

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const cors = (require("cors"))
+const http = require('http');
+const path = require('path');
+
 
 const app = express();
-app.use(cors());
+
+const port = process.env.PORT || 3001;
+
+
 app.use(express.json({ limit:"100mb"}));
 
 app.post('/api/send',(req, res) => {
@@ -13,13 +18,14 @@ app.post('/api/send',(req, res) => {
   });
 });
 
-app.get('/api/test',(req,res) => {
-  res.json({
-    'statusCode': 200,
-    'statusMessage': 'SUCCESS'
-});
+
+app.use(express.static(__dirname+'/dist/radar-web-page'));
+
+app.route('/*').get(function (req, res) {
+  return res.sendFile(path.join(__dirname+'/dist/radar-web-page'+'/index.html'));
 });
 
-app.listen(3000, (req,res) => {
-    console.log('Running');
-});
+const server = http.createServer(app);
+
+
+server.listen(port,() => console.log(__dirname));
