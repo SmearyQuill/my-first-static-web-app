1adc40632e932ba585ae386adfa4d8581fe82786
Raúl Andrés Argott Castro <raul.argott@aumenta.mx>
2022-04-01 18:10:23 -0600

    usando cors

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
-
+var cors = require('cors')
 
 const app = express();
 
 const port = process.env.PORT || 3001;
 
-
+app.use(cors());
 app.use(express.json({ limit:"100mb"}));
 
 app.post('/api/send',(req, res) => {
