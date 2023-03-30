40c939bb99a0483ade25397c2be582ebc7f8c709
Axel López Rodríguez <axel.lopez@aumenta.mx>
2023-03-30 17:56:35 -0600

    Botón para enviar whatsapp terminado.

--- src/app/Componentes/generals/whatsapp-floating-bubble/whatsapp-floating-bubble.component.ts
+++ src/app/Componentes/generals/whatsapp-floating-bubble/whatsapp-floating-bubble.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'whatsapp-floating-bubble',
   templateUrl: './whatsapp-floating-bubble.component.html',
-  styleUrls: ['./whatsapp-floating-bubble.component.css']
+  styleUrls: ['./whatsapp-floating-bubble.component.css'],
 })
 export class WhatsappFloatingBubbleComponent implements OnInit {
+  PhoneNumber: string = '4441951415';
+  Message: string = 'Hola, quiero información detallada sobre RADAR.';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  SendWhatsapp() {
+    window.open(
+      `http://web.whatsapp.com/send/?phone=521${this.PhoneNumber}&text=${this.Message}`,
+      'whatsapp'
+    );
   }
-
 }
