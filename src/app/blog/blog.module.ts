495834ccb817ffe2023c928fbf131ed017b5929e
Axel López Rodríguez <axel.lopez@aumenta.mx>
2023-05-08 16:23:05 -0600

    Inicio de seccion de tabs en blog.

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { BlogRoutingModule } from './blog-routing.module';
 import { IndexComponent } from './index/index.component';
 import { HeroComponent } from './hero/hero.component';
+import { PostGridComponent } from './post-grid/post-grid.component';
 
 
 @NgModule({
   declarations: [
     IndexComponent,
-    HeroComponent
+    HeroComponent,
+    PostGridComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/blog/post-grid/post-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'blog-post-grid',
+  templateUrl: './post-grid.component.html',
+  styleUrls: ['./post-grid.component.css']
+})
+export class PostGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
