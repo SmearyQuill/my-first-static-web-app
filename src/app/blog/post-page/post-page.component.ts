e63c0127dffd4e823321aa2946230ae7ba8a2d6d
Axel López Rodríguez <axel.lopez@aumenta.mx>
2023-05-10 14:05:41 -0600

    Cambio de color a navbar cuando esta en pantalla de blog.

--- src/app/blog/post-page/post-page.component.ts
+++ src/app/blog/post-page/post-page.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-post-page',
   templateUrl: './post-page.component.html',
-  styleUrls: ['./post-page.component.css']
+  styleUrls: ['./post-page.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class PostPageComponent implements OnInit {
-
-  constructor() { }
+export class PostPageComponent implements OnInit, OnDestroy {
+  Navbar = document.getElementById('menu-navbar');
+  constructor() {}
 
   ngOnInit(): void {
+    if (this.Navbar) {
+      console.log('Si funca');
+      this.Navbar.classList.remove('menu-navbar-color');
+      this.Navbar.classList.add('custom-navbar-color');
+    }
   }
 
+  ngOnDestroy(): void {
+    if (this.Navbar) {
+      this.Navbar.classList.remove('custom-navbar-color');
+      this.Navbar.classList.add('menu-navbar-color');
+    }
+  }
 }
