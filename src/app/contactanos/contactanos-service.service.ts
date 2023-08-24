c14ec5d3249d2bd4e2e2f1431294faec9b4b7a5d
Guillermo Mercado <guillermo.mercado@aumenta.mx>
2023-08-24 16:42:25 -0600

    add mail api call

--- src/app/contactanos/contactanos-service.service.ts
+++ src/app/contactanos/contactanos-service.service.ts
@@ -12,8 +12,11 @@ export class ContactanosServiceService {
   }
 
   sendMail(data: JSON): Observable<any> {
-    return this._http
+    this._http
       .post('/api/send-mail2',data,{observe:'response'})
+      .pipe(map((res) => res));    
+    return this._http
+      .post('/api/send-mail',data,{observe:'response'})
       .pipe(map((res) => res));
   }
 }
